4d37679c0ad980217f239fa3f66fbddb423d4ca2
Washington-coder <was.antonio.xy@gmail.com>
2026-03-24 15:49:24 -0400

    chore: add eslint/prettier config

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -3,10 +3,10 @@ import fastify from 'fastify'
 const app = fastify()
 
 app.get('/hello', () => {
-    return 'Hello World'
+  return 'Hello World'
 })
 
 // O listen no Fastify v4+ usa um objeto com a propriedade 'port'
 app.listen({ port: 3333 }).then(() => {
-    console.log('HTTP Server Running!')
+  console.log('HTTP Server Running!')
 })
