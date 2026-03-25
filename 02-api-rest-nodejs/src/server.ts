7e4f2372117ab0757809ea49ea2c1215fb57d022
Washington-coder <was.antonio.xy@gmail.com>
2026-03-25 16:17:04 -0400

    chore: add setup knex with sqlite3

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import { knex } from './database.js'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 // O listen no Fastify v4+ usa um objeto com a propriedade 'port'
