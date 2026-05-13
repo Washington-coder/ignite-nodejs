461d84d486901f7eac41383084da074c95ced997
Washington-coder <was.antonio.xy@gmail.com>
2026-05-12 23:19:02 -0400

    chore: add eslint and prettier config

--- 03-api-solid/src/server.ts
+++ 03-api-solid/src/server.ts
@@ -1,9 +1,15 @@
-import { app } from "./app.js";
-import { env } from "./env/index.js";
+import { app } from './app.js'
+import { env } from './env/index.js'
 
-app.listen({ 
+app
+  .listen({
     host: '0.0.0.0',
-    port: env.PORT
-}).then(() => {
-    console.log(" 🚀 Server is running on http://localhost:" + env.PORT);
-});
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`🚀 Server is running on http://localhost:${env.PORT}`)
+  })
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
