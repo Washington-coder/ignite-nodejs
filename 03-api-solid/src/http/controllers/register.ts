276898507d9e6edb0858e9b05eb8ac0ed9e47e03
Washington-coder <was.antonio.xy@gmail.com>
2026-05-15 16:48:32 -0400

    feat: add error handler for user already exists

--- 03-api-solid/src/http/controllers/register.ts
+++ 03-api-solid/src/http/controllers/register.ts
@@ -4,6 +4,7 @@ import type { FastifyRequest, FastifyReply } from 'fastify'
 import { hash } from 'bcryptjs'
 import { RegisterUseCase } from '../use-cases/register'
 import { PrismaUserRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '../use-cases/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -24,7 +25,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password,
     })
   } catch (err) {
-    return reply.status(409).send()
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
+
+    return reply.status(500).send() // TODO fix me later
   }
 
   return reply.status(201).send()
