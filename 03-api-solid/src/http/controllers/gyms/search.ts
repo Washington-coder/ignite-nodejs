56112f64f8145c27d54522cb0dd6ea07be1ce96e
Washington-coder <was.antonio.xy@gmail.com>
2026-08-05 14:40:45 -0400

    feat: add search, nearby controller for gyms

---
+++ 03-api-solid/src/http/controllers/gyms/search.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod'
+import type { FastifyRequest, FastifyReply } from 'fastify'
+import { makeSearchGymsUseCase } from '@/http/use-cases/factories/make-search-gyms-use-case'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { query, page } = searchGymsQuerySchema.parse(request.body)
+
+  const searchGymUseCase = makeSearchGymsUseCase()
+
+  const { gyms } = await searchGymUseCase.execute({
+    query,
+    page,
+  })
+
+  return reply.status(201).send({
+    gyms,
+  })
+}
