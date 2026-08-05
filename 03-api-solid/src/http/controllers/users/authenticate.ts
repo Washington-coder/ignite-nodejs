0ac4c45e5004cd11e081754ee4621ca8bfd0a2ac
Washington-coder <was.antonio.xy@gmail.com>
2026-08-05 14:26:39 -0400

    feat: add create gym controller

--- 03-api-solid/src/http/controllers/users/authenticate.ts
+++ 03-api-solid/src/http/controllers/users/authenticate.ts
@@ -1,10 +1,7 @@
 import { z } from 'zod'
 import type { FastifyRequest, FastifyReply } from 'fastify'
-import { PrismaUserRepository } from '@/repositories/prisma/prisma-users-repository'
-import { UserAlreadyExistsError } from '../use-cases/errors/user-already-exists-error'
-import { AuthenticateUseCase } from '../use-cases/authenticate'
-import { InvalidCredentialsError } from '../use-cases/errors/invalid-credentials-error'
-import { makeAuthenticateUseCase } from '../use-cases/factories/make-authenticate-use-case'
+import { InvalidCredentialsError } from './../../use-cases/errors/invalid-credentials-error'
+import { makeAuthenticateUseCase } from './../../use-cases/factories/make-authenticate-use-case'
 
 export async function authenticate(
   request: FastifyRequest,
